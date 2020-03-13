b2c481fce68941d1d1953bcaa3bc7527c4e9e3de
Sean Novick <sean.novick.ctr@us.af.mil>
2020-03-13 15:17:10 -0600

    Added another app

---
+++ website-blocker/app.py
@@ -0,0 +1,33 @@
+import time
+from datetime import datetime as dt
+
+hosts_temp = "website-blocker/hosts"
+hosts = open("C:\Windows\System32\drivers\etc\hosts")
+redirects = "127.0.0.1"
+website_list = [
+    "www.facebook.com",
+    "www.gmail.com",
+    "www.hotmail.com",
+    "www.twitter.com",
+    "www.instagram.com",
+]
+
+while True:
+    file = open(hosts_temp, "r+")
+    content = file.read()
+    now = dt.now()
+    if dt(now.year, now.month, now.day, 8) < now < dt(now.year, now.month, now.day, 16):
+        print("Work Hours...")
+        for site in website_list:
+            if site not in content:
+                file.write(redirects + "\t" + site + "\n")
+    else:
+        content = file.readlines()
+        file.seek(0)
+        for line in content:
+            if not any(site in line for site in website_list):
+                file.write(line)
+        file.truncate()
+        print("Let the fun times begin!")
+        break
+    time.sleep(10)
