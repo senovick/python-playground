111e391adf644791f1444b8dcc08ff0965edcadb
Sean Novick <sean.novick.ctr@us.af.mil>
2020-03-17 12:45:28 -0600

    Added functionality to Tkinter app buttons

--- desktop-app/app.py
+++ desktop-app/app.py
@@ -2,7 +2,6 @@ from tkinter import *
 from db import Db
 
 db = Db()
-db.connect()
 window = Tk("My Window")
 window.geometry("500x500")
 
@@ -11,7 +10,76 @@ author_text = StringVar()
 year_text = StringVar()
 isbn_text = StringVar()
 lb_text = StringVar()
-"""
+
+
+def view_all():
+    db.connect()
+    listBox.delete(0, END)
+    for row in db.get_books():
+        listBox.insert(END, row)
+
+
+def search_book():
+    db.connect()
+    listBox.delete(0, END)
+    search_term = db.search(
+        title_text.get(), author_text.get(), year_text.get(), isbn_text.get()
+    )
+    if type(search_term) is list:
+        for row in search_term:
+            listBox.insert(END, row)
+    else:
+        listBox.insert(END, search_term)
+
+
+def add_book():
+    db.connect()
+    result = db.insert(
+        title_text.get(), author_text.get(), year_text.get(), isbn_text.get()
+    )
+    listBox.insert(END, result)
+    clear_entries()
+
+
+def get_selected(event):
+    global selected_item
+    selected_item = listBox.get(listBox.curselection()[0])
+    populate_entries(selected_item)
+
+
+def delete_book():
+    db.connect()
+    db.delete(selected_item[0])
+    clear_entries()
+    view_all()
+
+
+def update_book():
+    db.connect()
+    db.update(
+        title_text.get(),
+        author_text.get(),
+        year_text.get(),
+        isbn_text.get(),
+        selected_item[0],
+    )
+    view_all()
+
+
+def populate_entries(item):
+    title_text.set(item[1])
+    author_text.set(item[2])
+    year_text.set(item[3])
+    isbn_text.set(item[4])
+
+
+def clear_entries():
+    title_text.set("")
+    author_text.set("")
+    year_text.set("")
+    isbn_text.set("")
+
+
 title_label = Label(window, text="Title")
 title_entry = Entry(window, textvariable=title_text)
 author_label = Label(window, text="Author")
@@ -20,14 +88,14 @@ year_label = Label(window, text="Year")
 year_entry = Entry(window, textvariable=year_text)
 isbn_label = Label(window, text="ISBN")
 isbn_entry = Entry(window, textvariable=isbn_text)
-listBox = Listbox(window, height=6, width=35)
+listBox = Listbox(window, height=6, width=40)
 sb = Scrollbar(window)
-viewBtn = Button(window, text="View All", width=12)
-searchBtn = Button(window, text="Search", width=12)
-addBtn = Button(window, text="Add", width=12)
-updateBtn = Button(window, text="Update", width=12)
-deleteBtn = Button(window, text="Delete", width=12)
-closeBtn = Button(window, text="Close", width=12)
+viewBtn = Button(window, text="View All", width=12, comman=view_all)
+searchBtn = Button(window, text="Search", width=12, command=search_book)
+addBtn = Button(window, text="Add", width=12, command=add_book)
+updateBtn = Button(window, text="Update", width=12, command=update_book)
+deleteBtn = Button(window, text="Delete", width=12, command=delete_book)
+closeBtn = Button(window, text="Close", width=12, command=window.destroy)
 title_label.grid(row=0, column=0)
 title_entry.grid(row=0, column=1)
 author_label.grid(row=0, column=2)
@@ -36,18 +104,17 @@ year_label.grid(row=1, column=0)
 year_entry.grid(row=1, column=1)
 isbn_label.grid(row=1, column=2)
 isbn_entry.grid(row=1, column=3)
-listBox.grid(row=2, column=0, rowspan=6, columnspan=2)
-sb.grid(row=2, column=2, rowspan=6)
+listBox.grid(row=2, column=0, rowspan=6, columnspan=3)
+sb.grid(row=2, column=3, rowspan=6)
 listBox.configure(yscrollcommand=sb.set)
+listBox.bind("<<ListboxSelect>>", get_selected)
 sb.configure(command=listBox.yview)
-viewBtn.grid(row=2, column=3)
-searchBtn.grid(row=3, column=3)
-addBtn.grid(row=4, column=3)
-updateBtn.grid(row=5, column=3)
-deleteBtn.grid(row=6, column=3)
-closeBtn.grid(row=7, column=3)
+viewBtn.grid(row=2, column=5)
+searchBtn.grid(row=3, column=5)
+addBtn.grid(row=4, column=5)
+updateBtn.grid(row=5, column=5)
+deleteBtn.grid(row=6, column=5)
+closeBtn.grid(row=7, column=5)
 
 
 window.mainloop()
-"""
-print(db.get_books())
--- desktop-app/db.py
+++ desktop-app/db.py
@@ -3,18 +3,19 @@ import sqlite3
 
 class Db:
     def __init__(self):
-        self.conn = None
-        self.cur = None
+        self.conn = sqlite3.connect("books.db")
+        self.cur = self.conn.cursor()
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS bookstore (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)"
+        )
+        self.conn.commit()
+        self.conn.close()
 
     def connect(self):
         try:
             self.conn = sqlite3.connect("books.db")
             self.cur = self.conn.cursor()
-            self.cur.execute(
-                "CREATE TABLE IF NOT EXISTS bookstore (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)"
-            )
-            self.conn.commit()
-            print("DATABASE CONNECTED")
+            print("Database Connected!")
         except sqlite3.DatabaseError as e:
             print("Problem Connecting to the Database")
             print(e)
@@ -27,14 +28,61 @@ class Db:
                 (title, author, year, isbn),
             )
             self.conn.commit()
-            print("data successfully inserted!")
+            # get last record inserted
+            self.cur.execute("SELECT * FROM bookstore ORDER BY id DESC LIMIT 1")
+            result = self.cur.fetchall()
         except sqlite3.DataError as e:
             print("There was a problem inserting the data")
             print(e)
         finally:
             self.conn.close()
+            return result
 
     def get_books(self):
         self.cur.execute("SELECT * from bookstore")
         result = self.cur.fetchall()
+        self.conn.close()
+        print("Database Closed!")
         return result
+
+    def get_book(self, id):
+        self.cur.execute("SELECT * FROM bookstore WHERE id=?", (id,))
+        result = self.cur.fetchall()
+        return result
+
+    def search(self, title="", author="", year="", isbn=""):
+        try:
+            self.cur.execute(
+                "SELECT * FROM bookstore WHERE title=? OR author=? OR year=? OR isbn=?",
+                (title, author, year, isbn),
+            )
+            result = self.cur.fetchall()
+            if len(result) == 0:
+                result = "Book not found"
+        except sqlite3.Error as e:
+            result = "Error getting books"
+        finally:
+            self.conn.close()
+        return result
+
+    def update(self, title, author, year, isbn, id):
+        try:
+            self.cur.execute(
+                "UPDATE bookstore SET title=?, author=?, year=?, isbn=? WHERE id=?",
+                (title, author, year, isbn, id),
+            )
+            self.conn.commit()
+        except sqlite3.Error as e:
+            print("Error Updating Book " + e)
+        finally:
+            self.conn.close()
+
+    def delete(self, id):
+        try:
+            self.cur.execute("DELETE FROM bookstore WHERE id=?", (id,))
+            self.conn.commit()
+            print("Book was successfully deleted!")
+        except sqlite3.Error as e:
+            print("An error occured deleting the book - {}".format(e))
+        finally:
+            self.conn.close()
